bfe0e5d5c48a0c35b6cd974a29984970ca1461b7
fraoustin <fraoustin@gmail.com>
2019-09-10 15:38:11 +0200

    <feat> color: color with association Annotation

--- app/custom/CustomRenderer.js
+++ app/custom/CustomRenderer.js
@@ -15,7 +15,7 @@ export default class CustomRenderer extends BaseRenderer {
   }
 
   canRender(element) {
-    return isAny(element, [ 'bpmn:Task',  'bpmn:Event', 'bpmn:DataStoreReference', 'bpmn:TextAnnotation']) && !element.labelTarget;
+    return isAny(element, [ 'bpmn:Task',  'bpmn:Event', 'bpmn:DataStoreReference', 'bpmn:TextAnnotation', 'bpmn:Association', 'bpmn:SequenceFlow']) && !element.labelTarget;
   }
 
   drawShape(parentNode, element) {
@@ -27,6 +27,21 @@ export default class CustomRenderer extends BaseRenderer {
  
   }
 
+  drawConnection(parentNode, element){
+    console.log(element)
+    const connection = this.bpmnRenderer.drawConnection(parentNode, element);
+    svgAttr(parentNode, {"type": element.type});
+    if (element.businessObject.eventDefinitions) {
+      svgAttr(parentNode, {"typedef" : element.businessObject.eventDefinitions[0].$type});
+    };    
+    if (element.businessObject.sourceRef) {
+      svgAttr(parentNode, {"typesrc" : element.businessObject.sourceRef.$type});
+    };    
+    if (element.businessObject.targetRef) {
+      svgAttr(parentNode, {"typetarget" : element.businessObject.targetRef.$type});
+    };    
+  
+  }
 }
 
 CustomRenderer.$inject = [ 'eventBus', 'bpmnRenderer' ];
